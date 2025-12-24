ede0dd412ac783722b52fc7221fd98279891658a
DayanaVan <19dayanavl06@mail.ru>
2025-12-24 21:04:27 +0700

    HashUtils.h

--- src/utils/HashUtils.h
+++ src/utils/HashUtils.h
@@ -4,25 +4,17 @@
 #include <string>
 #include <vector>
 
+using namespace std;
+
 class HashUtils {
 public:
-    // Генерация случайной соли
-    static std::string generateSalt(size_t length = 32);
-    
-    // Хеширование пароля с солью
-    static std::string hashPassword(const std::string& password, const std::string& salt);
-    
-    // Проверка пароля
-    static bool verifyPassword(const std::string& password, const std::string& hash, const std::string& salt);
-    
-    // Генерация случайного пароля
-    static std::string generateRandomPassword(size_t length = 12);
-    
-    // Проверка сложности пароля
-    static bool isPasswordStrong(const std::string& password);
-    
+    static string generateSalt(size_t length = 32);
+    static string hashPassword(const string& password, const string& salt);
+    static bool verifyPassword(const string& password, const string& hash, const string& salt);
+    static string generateRandomPassword(size_t length = 12);
+    static bool isPasswordStrong(const string& password);
 private:
-    static const std::string VALID_CHARS;
+    static const string VALID_CHARS;
 };
 
 #endif
